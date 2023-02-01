d33552e310b2a0007d712278b73ff73598467f5f
disco07 <koneenok@outlook.fr>
2023-02-01 03:53:43 +0100

    table cli

--- src/rct.rs
+++ src/rct.rs
@@ -70,22 +70,40 @@ impl Default for TableView {
 }
 
 impl Table<Stdout> {
-    /// Create a new ProgressBar with default configuration.
+    /// Create a new table CLI with default configuration.
     ///
     /// # Examples
     ///
     /// ```no_run
-    /// use std::thread;
-    /// use pbr::{ProgressBar, Units};
+    /// use std::collections::BTreeMap;
+    /// use rct::rct::Table;
     ///
-    /// let count = 1000;
-    /// let mut pb = ProgressBar::new(count);
-    /// pb.set_units(Units::Bytes);
+    /// let mut table = Table::new();
+    /// let mut data: Vec<BTreeMap<u32, String>> = vec![];
+    ///     data.push(BTreeMap::from([
+    ///         (1, "Drissa".to_string()),
+    ///         (2, "Kone".to_string()),
+    ///         (3, "07th April 1991".to_string()),
+    ///         (4, "Yes".to_string()),
+    ///         (5, "10 minutes".to_string()),
+    ///     ]));
+    ///     data.push(BTreeMap::from([
+    ///         (1, "Yaya".to_string()),
+    ///         (2, "Kone".to_string()),
+    ///         (3, "07th April 1991".to_string()),
+    ///         (4, "No".to_string()),
+    ///         (5, "9 minutes".to_string()),
+    ///     ]));
     ///
-    /// for _ in 0..count {
-    ///    pb.inc();
-    ///    thread::sleep_ms(100);
-    /// }
+    ///     table.add_field(1, "First Name");
+    ///     table.add_field(2, "Last Name");
+    ///     table.add_field(3, "DOB");
+    ///     table.add_field(4, "Admin");
+    ///     table.add_field(5, "Last Seen");
+    ///     table.add_data(data);
+    ///
+    ///     table.view()
+    ///
     /// ```
     pub fn new() -> Table<Stdout> {
         let handle = std::io::stdout();
@@ -260,31 +278,3 @@ impl<T: Write> Table<T> {
         printfl!(self.handle, "\r{}", self.create_table());
     }
 }
-
-// fn main() {
-//     let mut table = Table::new();
-//     let mut data: Vec<BTreeMap<u32, String>> = vec![];
-//     data.push(BTreeMap::from([
-//         (1, "Drissa".to_string()),
-//         (2, "Kone".to_string()),
-//         (3, "07th April 1991".to_string()),
-//         (4, "Yes".to_string()),
-//         (5, "10 minutes".to_string()),
-//     ]));
-//     data.push(BTreeMap::from([
-//         (1, "Yaya".to_string()),
-//         (2, "Kone".to_string()),
-//         (3, "07th April 1991".to_string()),
-//         (4, "No".to_string()),
-//         (5, "9 minutes".to_string()),
-//     ]));
-//
-//     table.add_field(1, "First Name");
-//     table.add_field(2, "Last Name");
-//     table.add_field(3, "DOB");
-//     table.add_field(4, "Admin");
-//     table.add_field(5, "Last Seen");
-//     table.add_data(data);
-//
-//     table.view()
-// }
