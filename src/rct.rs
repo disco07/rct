ae5db7a150abd2774c1f08e12d4d9924f102dba8
disco07 <koneenok@outlook.fr>
2023-02-01 03:40:20 +0100

    table cli

--- src/rct.rs
+++ src/rct.rs
@@ -261,30 +261,30 @@ impl<T: Write> Table<T> {
     }
 }
 
-fn main() {
-    let mut table = Table::new();
-    let mut data: Vec<BTreeMap<u32, String>> = vec![];
-    data.push(BTreeMap::from([
-        (1, "Drissa".to_string()),
-        (2, "Kone".to_string()),
-        (3, "07th April 1991".to_string()),
-        (4, "Yes".to_string()),
-        (5, "10 minutes".to_string()),
-    ]));
-    data.push(BTreeMap::from([
-        (1, "Yaya".to_string()),
-        (2, "Kone".to_string()),
-        (3, "07th April 1991".to_string()),
-        (4, "No".to_string()),
-        (5, "9 minutes".to_string()),
-    ]));
-
-    table.add_field(1, "First Name");
-    table.add_field(2, "Last Name");
-    table.add_field(3, "DOB");
-    table.add_field(4, "Admin");
-    table.add_field(5, "Last Seen");
-    table.add_data(data);
-
-    table.view()
-}
+// fn main() {
+//     let mut table = Table::new();
+//     let mut data: Vec<BTreeMap<u32, String>> = vec![];
+//     data.push(BTreeMap::from([
+//         (1, "Drissa".to_string()),
+//         (2, "Kone".to_string()),
+//         (3, "07th April 1991".to_string()),
+//         (4, "Yes".to_string()),
+//         (5, "10 minutes".to_string()),
+//     ]));
+//     data.push(BTreeMap::from([
+//         (1, "Yaya".to_string()),
+//         (2, "Kone".to_string()),
+//         (3, "07th April 1991".to_string()),
+//         (4, "No".to_string()),
+//         (5, "9 minutes".to_string()),
+//     ]));
+//
+//     table.add_field(1, "First Name");
+//     table.add_field(2, "Last Name");
+//     table.add_field(3, "DOB");
+//     table.add_field(4, "Admin");
+//     table.add_field(5, "Last Seen");
+//     table.add_data(data);
+//
+//     table.view()
+// }
