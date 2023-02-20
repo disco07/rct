e9de892fc46bff4e59d9435db08920e97d25314e
disco07 <koneenok@outlook.fr>
2023-02-20 09:44:17 +0100

    add comments

--- src/table.rs
+++ src/table.rs
@@ -1,17 +1,18 @@
 use crate::row::Row;
 use std::io::{Stdout, Write};
 
-// Macro for writing to the giving writer.
-// Used in both pb.rs and multi.rs modules.
-//
-// # Examples
-//
-// ```
-// let w = io::stdout();
-// printfl!(w, "");
-// printfl!(w, "\r{}", out);
-//
-// ```
+/// Macro for writing to the giving writer.
+/// Used in both pb.rs and multi.rs modules.
+///
+/// # Examples
+///
+/// ```
+/// use std::io;
+/// let w = io::stdout();
+/// printfl!(w, "");
+/// printfl!(w, "\r{}", out);
+///
+/// ```
 macro_rules! printfl {
    ($w:expr, $($tt:tt)*) => {{
         $w.write_all(&format!($($tt)*).as_bytes()).ok().expect("write() fail");
