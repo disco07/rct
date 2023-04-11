bca5f08a66646d2b376eff1066fb76e2c7b9a79b
disco07 <koneenok@outlook.fr>
2023-04-11 06:45:09 +0200

    derive

--- rct_tests/src/main.rs
+++ rct_tests/src/main.rs
@@ -12,6 +12,19 @@ struct Movies {
     created_at: String,
 }
 
+trait Ro {}
+
+impl Ro for Movies {
+    
+}
+
+impl<T> ITable for T 
+where T: Ro + 'static {
+    fn to_table(self) -> rct::Table {
+        unimplemented!()
+    }
+}
+
 fn main() {
     let movies = Movies {
         id: 1,
@@ -22,6 +35,17 @@ fn main() {
         description: "Movie".to_string(),
         created_at: "2023-01-01".to_string(),
     };
+    let vec_movies = vec![Movies {
+        id: 1,
+        title: "Harry Potter".to_string(),
+        is_enabled: false,
+        price: 20.0,
+        currency: "$".to_string(),
+        description: "Movie".to_string(),
+        created_at: "2023-01-01".to_string(),
+    }];
     let table = movies.to_table();
+    let v = vec_movies.t();
+    
     println!("{}", table.to_string());
 }
