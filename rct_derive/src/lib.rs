12a6e28ac98f3382501c200a9a4050be23be3653
disco07 <koneenok@outlook.fr>
2023-04-12 23:51:35 +0200

    derive

--- rct_derive/src/lib.rs
+++ rct_derive/src/lib.rs
@@ -1,13 +1,10 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro2::TokenTree;
+
 use quote::{quote, ToTokens};
-use syn::spanned::Spanned;
-use syn::{
-    parse_macro_input, Attribute, Data, DeriveInput, Error, Field, Fields, Ident, Lit, Meta,
-    MetaList, MetaNameValue, Path, PathSegment,
-};
+use syn::{Attribute, Data, DeriveInput, Error, Field, Fields, Ident, Meta, parse_macro_input, Token};
+use syn::punctuated::Punctuated;
 
 #[proc_macro_derive(ToTable, attributes(table))]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -98,15 +95,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 }
 
 fn get_attrs<'a>(field: &'a Field, attrs: &str) -> Option<&'a Attribute> {
-    while let Some(attr) = field.attrs.first() {
-        if let Meta::List(MetaList { path, .. }) = &attr.meta {
-            if !path.is_ident(attrs) {
-                return None;
-            }
-            return Some(attr);
-        }
-    }
-    None
+    field.attrs.iter().find(|a| { a.path().is_ident(attrs) })
 }
 
 
@@ -114,45 +103,17 @@ fn error_attr<T: ToTokens>(tokens: &T) -> proc_macro2::TokenStream {
     Error::new_spanned(tokens, "expected `table(bound = \"...\", ..)`").to_compile_error()
 }
 
-fn create_method(field: &Field, i: &Ident) -> proc_macro2::TokenStream {
-    if let Some(Attribute { meta, .. }) = get_attrs(field, "table") {
-        if let Meta::List(MetaList { tokens, .. }) = meta {
-            if let Some(TokenTree::Ident(i)) = tokens.clone().into_iter().next() {
-                if i != "each" {
-                    return error_attr(meta);
-                }
-            }
-            match tokens.clone().into_iter().nth(1).unwrap() {
-                TokenTree::Punct(p) => assert_eq!(p.as_char(), '='),
-                tt => panic!("expected '=' found {}", tt),
-            }
-            let literal = match tokens.clone().into_iter().nth(2).unwrap() {
-                TokenTree::Literal(l) => Lit::new(l),
-                tt => panic!("found {}", tt),
+fn create_method(field: &Field, i: &Ident) -> Result<proc_macro2::TokenStream, syn::Error> {
+    if let Some(attr) = get_attrs(field, "table") {
+        let nested = attr.parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)?;
+        for meta in nested {
+            let meta_list = match meta {
+                Meta::List(meta_list) => Ok(meta_list),
+                err => Err(Error::new_spanned(err, "unrecognized table")),
             };
-
-            match literal {
-                Lit::Str(s) => {
-                    let ident = Ident::new(&s.value(), field.span());
-                    if *i != ident {
-                        let name = &field.ident.clone().unwrap();
-                        let ty = inner_type(&field.ty, "Vec").unwrap();
-                        return quote! {
-                            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                                if let std::option::Option::Some(ref mut value) = self.#name {
-                                    value.push(#ident);
-                                } else {
-                                    self.#name = std::option::Option::Some(vec![#ident]);
-                                }
-                                self
-                            }
-                        };
-                    }
-                }
-                _ => unimplemented!(),
-            }
+            for nested_meta in meta_list.unwrap().parse_nested_meta().nested.into_iter() {}
         }
     }
 
-    proc_macro2::TokenStream::new()
+    Ok(proc_macro2::TokenStream::new())
 }
